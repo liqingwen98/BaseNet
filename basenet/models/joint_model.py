602ebd483878d653285f289180b6bc674b43665b
arwin <130128943+liqingwen98@users.noreply.github.com>
2024-06-05 14:23:12 +0800

    Update joint_model.py

--- basenet/models/joint_model.py
+++ basenet/models/joint_model.py
@@ -3,6 +3,7 @@ from funasr.losses.label_smoothing_loss import LabelSmoothingLoss
 from ..utils.model_utils import Feature_extract, Embeddings, PositionalEncoding
 from ..utils.mask_utils import generate_lower_triangular_mask, reverse_pad_list, make_pad_mask, add_sos_eos
 from ..utils.rescore_utils import ctc_beam_search
+from torch.nn.utils.rnn import pad_sequence
 
 class Model(torch.nn.Module):
     def __init__(self,
@@ -16,7 +17,8 @@ class Model(torch.nn.Module):
                 head = 8,
                 num_layers=3,
                 weight = 0.5,
-                ffd = 2048):
+                ffd = 2048, 
+                return_length = False):
         super().__init__()
 
         self.feature = Feature_extract(dim, stride)
@@ -46,6 +48,7 @@ class Model(torch.nn.Module):
         self.stride = stride
         self.weight = weight
         self.decode_method = decode_method
+        self.return_length = return_length
         
         self.ctc = torch.nn.CTCLoss()
         self.att = LabelSmoothingLoss(
@@ -56,48 +59,46 @@ class Model(torch.nn.Module):
         )
 
     def forward(self, 
-            speech: torch.Tensor,
-            speech_lengths: torch.Tensor,
-            text: torch.Tensor = None,
-            text_lengths: torch.Tensor = None,):
+            signal: torch.Tensor,
+            signal_lengths: torch.Tensor = None,
+            base: torch.Tensor = None,
+            base_lengths: torch.Tensor = None,):
         
-        if text is not None:
-            text = text[:, : text_lengths.max()]
-
-        speech = speech[:, :, :speech_lengths.max()]
-        encoder_out_lens = torch.ceil(speech_lengths/self.stride).long()
-        speech_masks = make_pad_mask(encoder_out_lens).bool().to(speech.device)
-        # if speech_masks.shape[-1] != 2000:
-        #     speech_masks = torch.cat((speech_masks, torch.ones(speech_masks.shape[0], 2000-speech_masks.shape[-1], dtype=torch.bool, device=speech.device)), dim=1)
+        if base is not None:
+            base = base[:, : base_lengths.max()]
+        if signal_lengths is not None:
+            signal = signal[:, :, :signal_lengths.max()]
+        else:
+            signal_lengths = torch.tensor([signal.shape[-1] for _ in range(signal.shape[0])])
+        encoder_out_lens = torch.ceil(signal_lengths/self.stride).long()
+        speech_masks = make_pad_mask(encoder_out_lens).bool().to(signal.device)
         
         # feature extract + position encoding
-        encoder_in = self.feature(speech)
+        encoder_in = self.feature(signal)
         encoder_in = self.position1(encoder_in)
         
         # Encoder
         encoder_inter = self.encoder(src=encoder_in, src_key_padding_mask=speech_masks)
-        # for mod in self.encoder:
-        #     encoder_in = mod(encoder_in, speech_masks)
         encoder_out = self.fc1(self.drop_out(encoder_inter))
         ctc_in = torch.nn.functional.log_softmax(encoder_out, -1).permute(1, 0, 2)
         
         if self.training:            
-            loss_ctc = self.ctc(ctc_in, text, encoder_out_lens, text_lengths)
+            loss_ctc = self.ctc(ctc_in, base, encoder_out_lens, base_lengths)
             # Decoder
             # prepare input data for decoder
-            text_x = torch.where(text == 0, torch.tensor(6, device=speech.device), text).int().to(speech.device)
+            text_x = torch.where(base == 0, torch.tensor(6, device=signal.device), base).int().to(signal.device)
 
             # prepare forward and reverse input
-            r_text_x = reverse_pad_list(text_x, text_lengths, float(self.pad))
+            r_text_x = reverse_pad_list(text_x, base_lengths, float(self.pad))
             ys_in, ys_out = add_sos_eos(text_x, self.sos, self.eos, self.pad)
             r_ys_in, r_ys_out = add_sos_eos(r_text_x, self.sos, self.eos, self.pad)
             ys_in = self.emb(ys_in)
             ys_in = self.position2(ys_in)
             r_ys_in = self.embr(r_ys_in)
             r_ys_in = self.position2r(r_ys_in)
-            text_lengths = text_lengths + 1
-            tgt_pad_mask = make_pad_mask(text_lengths).bool().to(text.device)
-            tgt_mask = generate_lower_triangular_mask(ys_in.shape[1]).bool().to(text.device)
+            base_lengths = base_lengths + 1
+            tgt_pad_mask = make_pad_mask(base_lengths).bool().to(base.device)
+            tgt_mask = generate_lower_triangular_mask(ys_in.shape[1]).bool().to(base.device)
 
             decoder_out = self.decoder(tgt=ys_in, memory=encoder_inter, tgt_mask=~tgt_mask, tgt_key_padding_mask=tgt_pad_mask, memory_key_padding_mask=speech_masks)
             decoder_out = self.fc2(decoder_out)
@@ -110,7 +111,10 @@ class Model(torch.nn.Module):
             return loss
         else:
             if self.decode_method == 'ctc':
-                return ctc_in, encoder_out_lens
+                if self.return_length:
+                    return ctc_in, encoder_out_lens
+                else:
+                    return ctc_in
             elif self.decode_method == 'rescore':
                 i = 0
                 seq = []
@@ -122,7 +126,7 @@ class Model(torch.nn.Module):
                     seq_len = [len(item[0]) for item in hyps]
                     inter_len = torch.tensor(seq_len)
                     inter_seq = [torch.tensor(i) for i in inter_seq]
-                    inter_seq = pad_sequence(inter_seq, batch_first=True, padding_value=6).to(speech.device)
+                    inter_seq = pad_sequence(inter_seq, batch_first=True, padding_value=6).to(signal.device)
 
                     # prepare forward and reverse input
                     r_inter_seq = reverse_pad_list(inter_seq, inter_len, float(self.pad))
@@ -133,8 +137,8 @@ class Model(torch.nn.Module):
                     r_ys_in = self.embr(r_ys_in)
                     r_ys_in = self.position2r(r_ys_in)
                     inter_len = inter_len + 1
-                    tgt_pad_mask = make_pad_mask(inter_len).bool().to(speech.device)
-                    tgt_mask = generate_lower_triangular_mask(ys_in.shape[1]).bool().to(speech.device)
+                    tgt_pad_mask = make_pad_mask(inter_len).bool().to(signal.device)
+                    tgt_mask = generate_lower_triangular_mask(ys_in.shape[1]).bool().to(signal.device)
 
                     # decode
                     decoder_out = self.decoder(tgt=ys_in, memory=encoder_inter[i].unsqueeze(0).repeat(self.beam_size, 1, 1), 
